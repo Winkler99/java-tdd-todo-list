8af3613b20110474a7efc6646694a493335e4abf
Wilmer Winkler <wilmer.winkler@se.experis.com>
2025-01-09 14:19:57 +0100

    Tests for add() function complete

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -5,9 +5,30 @@ import org.junit.jupiter.api.Test;
 
 class TodoListTest {
     @Test
-    public void exampleTest() {
-        String hello = "Hello";
-        Assertions.assertEquals("Hello", hello);
-        Assertions.assertNotEquals("Goodbye", hello);
+    public void addTaskToListThatAlreadyExist() {
+        TodoList todoList = new TodoList();
+        String taskName = "clean";
+
+        todoList.add("clean");
+        todoList.add("wash");
+        todoList.add("vacuum");
+        todoList.add("walk the dog");
+
+        Assertions.assertFalse(todoList.add(taskName));
     }
+
+    @Test
+    public void addTaskToListThatDoesNotAlreadyExist() {
+        TodoList todoList = new TodoList();
+        String taskName = "run";
+
+        todoList.add("clean");
+        todoList.add("wash");
+        todoList.add("vacuum");
+        todoList.add("walk the dog");
+
+        Assertions.assertTrue(todoList.add(taskName));
+    }
+
+
 }
