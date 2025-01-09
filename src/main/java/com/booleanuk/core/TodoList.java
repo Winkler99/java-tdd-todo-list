808747a77ebbb6da33214a4a22ae96268c7dde4c
Wilmer Winkler <wilmer.winkler@se.experis.com>
2025-01-09 14:25:56 +0100

    Code for add() function complete and passed the tests.

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,16 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+
 public class TodoList {
 
+    public HashMap<String, Boolean> tasks = new HashMap<>();
+
+    public boolean add(String taskName){
+        if(tasks.containsKey(taskName)){
+            return false;
+        }
+        tasks.put(taskName, false);
+        return true;
+    }
 }
